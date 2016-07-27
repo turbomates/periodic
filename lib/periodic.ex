9a8e1872e161543787e29209bf9b3edb78491e41
driver <driver.devel@gmail.com>
2016-07-27 18:37:14 +0300

    Add ability to execute functions periodically

--- lib/periodic.ex
+++ lib/periodic.ex
@@ -1,2 +1,18 @@
 defmodule Periodic do
+  use Periodic.Worker
+
+  def work({m, f, a}) do
+    {:ok, new_args} = apply(m, f, a)
+    {:ok, {m, f, new_args}}
+  end
+
+  def work({f, a}) do
+    {:ok, new_args} = apply(f, a)
+    {:ok, {f, new_args}}
+  end
+
+  def work(f) when is_function(f) do
+    f.()
+    {:ok, f}
+  end
 end
--- test/periodic_test.exs
+++ test/periodic_test.exs
@@ -1,8 +1,26 @@
 defmodule PeriodicTest do
-  use ExUnit.Case
-  doctest Periodic
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def periodic_func(pid) do
+    send(pid, :from_periodic)
+    {:ok, [pid]}
+  end
+
+  test "periodic function call" do
+    parent = self()
+    Periodic.start_link(fn -> send(parent, :from_periodic) end)
+    assert_receive :from_periodic
+  end
+
+  test "periodic function with args call" do
+    parent = self()
+    Periodic.start_link({fn m -> send(parent, m); {:ok, m}; end, [:from_periodic]})
+    assert_receive :from_periodic
+  end
+
+  test "periodic module function call with args" do
+    parent = self()
+    Periodic.start_link({PeriodicTest, :periodic_func, [parent]})
+    assert_receive :from_periodic
   end
 end
