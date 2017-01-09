0dbc22c4b497e187e9f6b1fccd87bef1ebb5dbf5
driver <driver.devel@gmail.com>
2017-01-09 18:14:55 +0300

    Fix 1.4 warnings

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Periodic.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     aliases: aliases,
+     aliases: aliases(),
      deps: deps()]
   end
 
